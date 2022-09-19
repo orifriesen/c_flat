0f7657714959185b81d99958489c424204613a33
bean-b <bbaumsta@macalester.edu>
2022-09-19 11:32:27 -0500

    balls Destroy themselves

--- Assets/test.cs
+++ Assets/test.cs
@@ -13,6 +13,15 @@ public class test : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(!GetComponent<Renderer>().isVisible){
+            Destroy(gameObject);
+        }
     }
+<<<<<<< Updated upstream:Assets/test.cs
+=======
+    
+    private void OnCollisionEnter2D(Collision2D other) {
+        other.collider.SendMessage("PlaySound");
+    }
+>>>>>>> Stashed changes:Assets/Scripts/Ball.cs
 }
