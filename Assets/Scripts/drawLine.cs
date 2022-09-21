ee7e21130bdbfe1ba31f72f47d90db8196b3bf49
Ori Friesen <32725393+orifriesen@users.noreply.github.com>
2022-09-21 11:50:11 -0500

    fixed branch stuff

---
+++ Assets/Scripts/drawLine.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class drawLine : MonoBehaviour
+{
+    Vector3 initMousePos, finalMousePos;
+    public LineRenderer line;
+    public List<LineRenderer> musicLines = new List<LineRenderer>();
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            initMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            initMousePos.z = Camera.main.nearClipPlane;
+            musicLines.Add(Instantiate(line));
+            Debug.Log("click");
+        }
+
+        if(Input.GetMouseButton(0)) {
+            finalMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            finalMousePos.z = Camera.main.nearClipPlane;
+            musicLines.Last().SetPosition(0, initMousePos);
+            musicLines.Last().SetPosition(1, finalMousePos);
+        }
+    }
+}
