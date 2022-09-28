2145545594c1ae2c1c63ad2a54f91c25dcc7b3be
Ori Friesen <32725393+orifriesen@users.noreply.github.com>
2022-09-28 11:46:30 -0500

    added line script

--- Assets/Scripts/lineScript.cs
+++ Assets/Scripts/lineScript.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class lineScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void OnMouseOver() {
+        if (Input.GetMouseButtonDown(1)) {
+            Debug.Log("delete");
+        }
     }
 }
