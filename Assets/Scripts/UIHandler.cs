cccdf2f673e30519a5e1bd421b70429f355e0c0e
Ori Friesen <32725393+orifriesen@users.noreply.github.com>
2022-10-07 11:51:02 -0500

    made it so cannot draw lines over ui

--- Assets/Scripts/UIHandler.cs
+++ Assets/Scripts/UIHandler.cs
@@ -1,26 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 public class UIHandler : MonoBehaviour
 { 
     public Button resetButton;
     public BallSpawner ballSpawner;
     private drawLine lineDrawer;
+    private int UILayer;
+    private bool isButtonDownOnUI = false;
     
     void Start(){
         resetButton.onClick.AddListener(ResetOnClick);
         lineDrawer = GetComponent<drawLine>();
+        UILayer = LayerMask.NameToLayer("UI");
     }
 
     void Update(){
-        if (Input.GetMouseButtonDown(0)) {
+        if(Input.GetMouseButton(0) && IsPointerOverUIElement()) {
+            isButtonDownOnUI = true;
+        }
+        if(isButtonDownOnUI == true && Input.GetMouseButtonUp(0)) {
+            isButtonDownOnUI = false;
+        }
+        if (Input.GetMouseButtonDown(0) && !isButtonDownOnUI) {
             lineDrawer.startLine();
-        }else if(Input.GetMouseButton(0)){
+        }else if(Input.GetMouseButton(0) && !isButtonDownOnUI){
             lineDrawer.finishLine();
-        }else if(Input.GetMouseButtonDown(1)){
+        }else if(Input.GetMouseButtonDown(1) && !isButtonDownOnUI){
             lineDrawer.destroyIfAt(Camera.main.ScreenToWorldPoint(Input.mousePosition));
-        }else if(Input.GetKeyDown("b")){
+        }else if(Input.GetKeyDown("b") && !isButtonDownOnUI){
             Vector3 mosPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             BallSpawner b = Instantiate(ballSpawner, new Vector3(mosPos.x, mosPos.y, 0), ballSpawner.transform.localRotation);
         }
@@ -36,4 +46,34 @@ public class UIHandler : MonoBehaviour
             }
             lineDrawer.DestroyAll();   
 	}
+    //Returns 'true' if we touched or hovering on Unity UI element.
+    public bool IsPointerOverUIElement()
+    {
+        return IsPointerOverUIElement(GetEventSystemRaycastResults());
+    }
+ 
+ 
+    //Returns 'true' if we touched or hovering on Unity UI element.
+    private bool IsPointerOverUIElement(List<RaycastResult> eventSystemRaysastResults)
+    {
+        for (int index = 0; index < eventSystemRaysastResults.Count; index++)
+        {
+            RaycastResult curRaysastResult = eventSystemRaysastResults[index];
+            if (curRaysastResult.gameObject.layer == UILayer)
+                return true;
+        }
+        return false;
+    }
+ 
+ 
+    //Gets all event system raycast results of current mouse or touch position.
+    static List<RaycastResult> GetEventSystemRaycastResults()
+    {
+        PointerEventData eventData = new PointerEventData(EventSystem.current);
+        eventData.position = Input.mousePosition;
+        List<RaycastResult> raysastResults = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventData, raysastResults);
+        return raysastResults;
+    }
+
 }
