b6f162a009e41e424e34b5aa0346248dac78e335
bean-b <bbaumsta@macalester.edu>
2022-10-17 12:43:27 -0500

    small

--- Assets/Scripts/SpeedSlider.cs
+++ Assets/Scripts/SpeedSlider.cs
@@ -7,12 +7,12 @@ public class SpeedSlider : MonoBehaviour
 
     private BallSpawner ballSpawner;
     public void setValFromDelay(float delay){
-        this.gameObject.GetComponent<Slider>().value = (float)(delay /2.5 -.1);
+        this.gameObject.GetComponent<Slider>().value = (float)(delay /3.0 -.1);
     }
     public void SetBallSpawner(BallSpawner bs){
         ballSpawner = bs;
     }
     private void Update() {
-        ballSpawner.delay = (float)((this.gameObject.GetComponent<Slider>().value + .1)*2.5);
+        ballSpawner.delay = (float)(this.gameObject.GetComponent<Slider>().value + .1)*3;
     }
 }
