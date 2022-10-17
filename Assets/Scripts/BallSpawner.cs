4cae1ac19f0d435f5b92148c60d5650ca3b05a0f
bean-b <bbaumsta@macalester.edu>
2022-10-17 14:55:39 -0500

    small

--- Assets/Scripts/BallSpawner.cs
+++ Assets/Scripts/BallSpawner.cs
@@ -39,9 +39,9 @@ public class BallSpawner : MonoBehaviour
         float yOffset = (pos.y + 0.5f > maxY) ? -.50f : .50f;
         float xOffset = 0;
         if(pos.x +.75f > maxX){
-            xOffset = (maxX-pos.x) -1f;
+            xOffset = (maxX-pos.x) -.9f;
         }else if(pos.x -.75f < minX){
-            xOffset = (minX-pos.x) +1f;
+            xOffset = (minX-pos.x) +1.3f;
         }
 
         GameObject speedSlider = Instantiate(speedSliderExample, Camera.main.ScreenToWorldPoint(this.transform.position), this.transform.localRotation);
