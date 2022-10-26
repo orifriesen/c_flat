6f9e9d0c2c7d6b675f4fa6448a2a72d914b74572
bean-b <bbaumsta@macalester.edu>
2022-10-26 11:28:04 -0500

    home screen start, particle system change, trash button change

---
+++ Assets/Scripts/HomeScreenScript.cs
@@ -0,0 +1,19 @@
+     using System.Collections;
+     using System.Collections.Generic;
+     using UnityEngine;
+     using UnityEngine.SceneManagement;
+
+public class HomeScreenScriot : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SceneManager.LoadScene("MyScene");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
