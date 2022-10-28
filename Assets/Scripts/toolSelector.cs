9035f2916c45101e014261267737d3ce4f391a05
Ori Friesen <32725393+orifriesen@users.noreply.github.com>
2022-10-28 11:52:47 -0500

    added bass ui

--- Assets/Scripts/toolSelector.cs
+++ Assets/Scripts/toolSelector.cs
@@ -6,8 +6,8 @@ using System;
 public class toolSelector : MonoBehaviour
 {
     //TODO turn this into an array based approach for code neatness
-    public Button guitar1, guitar2, guitar3, piano1, piano2, piano3;
-    public Material guitar1m, guitar2m, guitar3m, piano1m, piano2m, piano3m;
+    public Button guitar1, guitar2, guitar3, piano1, piano2, piano3, bass1, bass2, bass3;
+    public Material guitar1m, guitar2m, guitar3m, piano1m, piano2m, piano3m, bass1m, bass2m, bass3m;
     public static Material lineMaterial;
     // Start is called before the first frame update
     void Start()
@@ -18,8 +18,16 @@ public class toolSelector : MonoBehaviour
         piano1.onClick.AddListener(Piano1Click);
         piano2.onClick.AddListener(Piano2Click);
         piano3.onClick.AddListener(Piano3Click);
+<<<<<<< Updated upstream
         lineMaterial = piano1m;
         piano1.interactable = false;
+=======
+        bass1.onClick.AddListener(Bass1Click);
+        bass2.onClick.AddListener(Bass2Click);
+        bass3.onClick.AddListener(Bass3Click);
+        lineMaterial = piano2m;
+        piano2.interactable = false;
+>>>>>>> Stashed changes
     }
 
     void Guitar1Click() {
@@ -46,6 +54,18 @@ public class toolSelector : MonoBehaviour
         lineMaterial = piano3m;
         select(piano3);
     }
+    void Bass1Click() {
+        lineMaterial = bass1m;
+        select(bass1);
+    }
+    void Bass2Click() {
+        lineMaterial = bass2m;
+        select(bass2);
+    }
+    void Bass3Click() {
+        lineMaterial = bass3m;
+        select(bass3);
+    }
 
     void select(Button button) {
         guitar1.interactable = true;
@@ -54,6 +74,9 @@ public class toolSelector : MonoBehaviour
         piano1.interactable = true;
         piano2.interactable = true;
         piano3.interactable = true;
+        bass1.interactable = true;
+        bass2.interactable = true;
+        bass3.interactable = true;
         button.interactable = false;
     }
 
@@ -68,8 +91,40 @@ public class toolSelector : MonoBehaviour
             return 3;
         }if(lineMaterial == piano2){
             return 4;
+<<<<<<< Updated upstream
         }if(lineMaterial == piano3){
             return 5;
+=======
+        }if(lineMaterial == piano1m){
+            return 1.4;
+        }if(lineMaterial == piano2m){
+            return 3;
+        }if(lineMaterial == piano3m){
+            return 4.6;
+        }
+        throw new Exception("this should'nt happen, toolSelector getColorInt()");
+    }
+
+    public Color getColor(){
+        if(lineMaterial == guitar1m){
+            return guitar1.GetComponent<Image>().color;
+        }if(lineMaterial == guitar2m){
+            return guitar2.GetComponent<Image>().color;;
+        }if(lineMaterial == guitar3m){
+            return guitar3.GetComponent<Image>().color;;
+        }if(lineMaterial == piano1m){
+            return piano1.GetComponent<Image>().color;;
+        }if(lineMaterial == piano2m){
+            return piano2.GetComponent<Image>().color;;
+        }if(lineMaterial == piano3m){
+            return piano3.GetComponent<Image>().color;;
+        }if(lineMaterial == bass1m){
+            return bass1.GetComponent<Image>().color;;
+        }if(lineMaterial == bass2m){
+            return bass2.GetComponent<Image>().color;;
+        }if(lineMaterial == bass3m){
+            return bass3.GetComponent<Image>().color;;
+>>>>>>> Stashed changes
         }
         throw new Exception("this should'nt happen, toolSelector getColorInt");
     }
