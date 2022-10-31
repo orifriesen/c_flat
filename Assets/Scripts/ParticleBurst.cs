52bb689d9d786be30ff0df81bcd1de35e82e08f8
bean-b <bbaumsta@macalester.edu>
2022-10-31 11:32:57 -0500

    gravity icon, particle bug fix

--- Assets/Scripts/ParticleBurst.cs
+++ Assets/Scripts/ParticleBurst.cs
@@ -7,7 +7,7 @@ public class ParticleBurst : MonoBehaviour
     public ParticleSystem collisionParticleSystem;
 
     //Triggers the particle effect if it collides with a line
-    void OnTriggerEnter2D(Collider2D other) {
+    void OnTriggerExit2D(Collider2D other) {
         if(other.gameObject.CompareTag("line")){
             var em = collisionParticleSystem.emission;
             em.enabled = true;
